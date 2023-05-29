a7584074a56c7557fbd42711c58ba28a82112f90
ozgurseker <ozgursheker@gmail.com>
2023-05-29 01:04:15 -0400

    volume indicator update

--- getdailydata.py
+++ getdailydata.py
@@ -22,6 +22,9 @@ new_df = pd.DataFrame()
 ema14 = []
 sma55 = []
 sma200 = []
+vsma20 = []
+vsma5 = []
+vol = []
 for stock in existingdata["Symbol"]:
     print(stock)
     data = yf.Ticker(name_fix(stock)).history(period = "2y")
@@ -31,8 +34,18 @@ for stock in existingdata["Symbol"]:
     ema14.append(ema)
     sma55.append(sma1)
     sma200.append(sma2)
+    vsma1 = data["Volume"].rolling(window = 20).mean()[-1]
+    vsma2= data["Volume"].rolling(window = 5).mean()[-1]
+    volume = data["Volume"].rolling(window = 1).mean()[-1]
+    vsma20.append(vsma1)
+    vsma5.append(vsma2)
+    vol.append(volume)
 
 existingdata["ema14"] = ema14
 existingdata["ma55"] = sma55
 existingdata["ma200"] = sma200
+existingdata["vsma20"] = vsma20
+existingdata["vsma5"] = vsma5
+existingdata["volume"] = vol
+
 existingdata.to_csv("data.csv",  index = False)
